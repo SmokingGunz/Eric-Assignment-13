927a7aa12be7d070a972984dc5f9b8349aaf7527
SmokingGunz <ericowens08@gmail.com>
2023-07-21 06:42:10 -0700

    Still fighting the issue with updating user information. Now if I don't update the password it will delete it from the database

--- src/main/java/com/coderscampus/Assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/Assignment13/web/UserController.java
@@ -21,60 +21,74 @@ import com.coderscampus.Assignment13.service.UserService;
 @Controller
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private AccountService accountService;
+    @Autowired
+    private AccountService accountService;
 
-	@Autowired
-	private AddressService addressService;
+    @Autowired
+    private AddressService addressService;
 
-	@GetMapping("/register")
-	public String getCreateUser(ModelMap model) {
+    @GetMapping("/register")
+    public String getCreateUser(ModelMap model) {
 
-		model.put("user", new User());
+        model.put("user", new User());
 
-		return "register";
-	}
+        return "register";
+    }
 
-	@PostMapping("/register")
-	public String postCreateUser(User user) {
-		System.out.println(user);
-		userService.saveUser(user);
-		return "redirect:/register";
-	}
+    @PostMapping("/register")
+    public String postCreateUser(User user) {
+        System.out.println(user);
+        userService.saveUser(user);
+        return "redirect:/register";
+    }
 
-	@GetMapping("/users")
-	public String getAllUsers(ModelMap model) {
-		Set<User> users = userService.findAll();
+    @GetMapping("/users")
+    public String getAllUsers(ModelMap model) {
+        Set<User> users = userService.findAll();
 
-		model.put("users", users);
-		if (users.size() == 1) {
-			model.put("user", users.iterator().next());
-		}
+        model.put("users", users);
+        if (users.size() == 1) {
+            model.put("user", users.iterator().next());
+        }
 
-		return "users";
-	}
+        return "users";
+    }
 
-	@GetMapping("/users/{userId}")
-	public String getOneUser(ModelMap model, @PathVariable Long userId) {
-		User user = userService.findById(userId);
-		List<Account> account = user.getAccounts();
+    @GetMapping("/users/{userId}")
+    public String getOneUser(ModelMap model, @PathVariable Long userId) {
+        User user = userService.findById(userId);
+        List<Account> account = user.getAccounts();
 
-		if (user.getAddress() == null) {
-			Address address = new Address();
-			address.setUser(user);
-			address.setUserId(userId);
-			user.setAddress(address);
-		}
+        if (user.getAddress() == null) {
+            Address address = new Address();
+            address.setUser(user);
+            address.setUserId(userId);
+            user.setAddress(address);
+        }
 
-		model.put("users", Arrays.asList(user));
-		model.put("user", user);
-		model.put("accounts", account);
-		model.put("address", user.getAddress());
-		return "users";
-	}
+        model.put("users", Arrays.asList(user));
+        model.put("user", user);
+        model.put("accounts", account);
+        model.put("address", user.getAddress());
+        return "users";
+    }
+
+    // PostMapping method to update a user and address in the database then redirect to the users page
+    @PostMapping("/users/{userId}")
+    public String postOneUser(@PathVariable Long userId, User newUser) {
+        Address address = newUser.getAddress(); // Retrieve the existing Address from the
+        User oldUser = userService.findByIdWithAccounts(userId); // Retrieve the existing User from the database
+        newUser.setAccounts(oldUser.getAccounts());
+        newUser.setAddress(address);
+		newUser.setUserId(userId);
+		address.setUser(newUser);
+        address.setUserId(userId);
+        userService.saveUser(newUser);
+        return "redirect:/users/" + userId;
+    }
 
 //	@PostMapping("/users/{userId}")
 //	public String postUser(@PathVariable Long userId, User user) {
@@ -136,12 +150,12 @@ public class UserController {
 //
 //	    return "redirect:/users/" + userId;
 //	}
-	
 
-	@PostMapping("/users/{userId}/delete")
-	public String deleteOneUser(@PathVariable Long userId) {
-		userService.delete(userId);
-		return "redirect:/users";
-	}
+
+    @PostMapping("/users/{userId}/delete")
+    public String deleteOneUser(@PathVariable Long userId) {
+        userService.delete(userId);
+        return "redirect:/users";
+    }
 
 }
