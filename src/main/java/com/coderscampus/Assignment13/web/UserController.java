d501d6a28e8d116cc2573bd12e5b2f202b117231
SmokingGunz <ericowens08@gmail.com>
2023-07-24 00:51:09 -0700

    fixed password saving issue and cleaned up controller methods.

--- src/main/java/com/coderscampus/Assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/Assignment13/web/UserController.java
@@ -19,135 +19,68 @@ import com.coderscampus.Assignment13.service.UserService;
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @GetMapping("/register")
-    public String getCreateUser(ModelMap model) {
+	@GetMapping("/register")
+	public String getCreateUser(ModelMap model) {
 
-        model.put("user", new User());
+		model.put("user", new User());
 
-        return "register";
-    }
+		return "register";
+	}
 
-    @PostMapping("/register")
-    public String postCreateUser(User user) {
-        System.out.println(user);
-        userService.saveUser(user);
-        return "redirect:/register";
-    }
+	@PostMapping("/register")
+	public String postCreateUser(User user) {
+		System.out.println(user);
+		userService.saveUser(user);
+		return "redirect:/register";
+	}
 
-    @GetMapping("/users")
-    public String getAllUsers(ModelMap model) {
-        Set<User> users = userService.findAll();
+	@GetMapping("/users")
+	public String getAllUsers(ModelMap model) {
+		Set<User> users = userService.findAll();
 
-        model.put("users", users);
-        if (users.size() == 1) {
-            model.put("user", users.iterator().next());
-        }
+		model.put("users", users);
+		if (users.size() == 1) {
+			model.put("user", users.iterator().next());
+		}
 
-        return "users";
-    }
+		return "users";
+	}
 
-    @GetMapping("/users/{userId}")
-    public String getOneUser(ModelMap model, @PathVariable Long userId) {
-        User user = userService.findById(userId);
-        List<Account> account = user.getAccounts();
+	@GetMapping("/users/{userId}")
+	public String getOneUser(ModelMap model, @PathVariable Long userId) {
+		User user = userService.findById(userId);
+//		List<Account> account = user.getAccounts();
 
-        if (user.getAddress() == null) {
-            Address address = new Address();
-            address.setUser(user);
-            address.setUserId(userId);
-            user.setAddress(address);
-        }
+//		if (user.getAddress() == null) {
+//			Address address = new Address();
+//			address.setUser(user);
+//			address.setUserId(userId);
+//			user.setAddress(address);
+//		}
 
-        model.put("users", Arrays.asList(user));
-        model.put("user", user);
-        model.put("accounts", account);
-        model.put("address", user.getAddress());
-        return "users";
-    }
+		model.put("users", Arrays.asList(user));
+		model.put("user", user);
+//		model.put("accounts", account);
+//		model.put("address", user.getAddress());
+		return "users";
+	}
 
-    // PostMapping method to update a user and address in the database then redirect to the users page
-    @PostMapping("/users/{userId}")
-    public String postOneUser(@PathVariable Long userId, User newUser) {
-        Address address = newUser.getAddress(); // Retrieve the existing Address from the
-        User oldUser = userService.findByIdWithAccounts(userId); // Retrieve the existing User from the database
-        newUser.setAccounts(oldUser.getAccounts());
-        newUser.setAddress(address);
-		newUser.setUserId(userId);
-		address.setUser(newUser);
-        address.setUserId(userId);
-        userService.saveUser(newUser);
-        return "redirect:/users/" + userId;
-    }
+	// PostMapping method to update a user and address in the database then redirect
+	// to the users page
+	@PostMapping("/users/{userId}")
+	public String postOneUser(@PathVariable Long userId, User user) {
 
-//	@PostMapping("/users/{userId}")
-//	public String postUser(@PathVariable Long userId, User user) {
-//
-//		User oldUser = userService.findByIdWithAccounts(userId); // Retrieve the existing User from the database
-//		user.setAccounts(oldUser.getAccounts());
-//		Address address = addressService.save(user.getAddress()); // Retrieve the existing Address from the database
-//		user.setAddress(address);
-//
-//		address.setUser(oldUser);
-//		user.setAddress(address);
-//		address.setUserId(userId);
-////
-////		addressService.save(address); // Save the updated Address object
-//
-//		userService.saveUser(user);
-//
-//		return "redirect:/users/" + userId;
-//	}
+		userService.saveUser(user);
+		return "redirect:/users/" + userId;
+	}
 
-//	@PostMapping("/users/{userId}")
-//	public String postOneUser(@PathVariable Long userId, User newUser) {
-//		User oldUser = userService.findById(userId); // Retrieve the existing User from the database
-//
-//		// Get the updated Address from the form
-//		Address updatedAddress = newUser.getAddress();
-//
-//		// Update properties of oldUser with properties from newUser if they are not
-//		// null
-//		userService.updateUserProperties(oldUser, newUser);
-//
-//		// Update properties of the associated Address with properties from
-//		// updatedAddress if they are not null
-//		userService.updateAddressProperties(oldUser.getAddress(), updatedAddress);
-//
-//		userService.saveUser(oldUser); // Save the updated User object
-//
-//		return "redirect:/users/" + userId;
-//	}
-
-//	@PostMapping("/users/{userId}")
-//	public String postUser(@PathVariable Long userId, User user) {
-//	    User oldUser = userService.findById(userId); // Retrieve the existing User from the database
-//	    user.setAccounts(oldUser.getAccounts());
-//
-//	    // Get the updated Address from the form
-//	    Address updatedAddress = user.getAddress();
-//	    if (updatedAddress == null) {
-//	        updatedAddress = new Address();
-//	    } else {
-//	        // Retrieve the existing Address associated with the user
-//	        Address address = oldUser.getAddress();
-//	    }
-//
-//	    user.setAddress(updatedAddress);
-//	    updatedAddress.setUser(user);
-//
-//	    userService.saveUser(user); // Save the updated User object
-//
-//	    return "redirect:/users/" + userId;
-//	}
-
-
-    @PostMapping("/users/{userId}/delete")
-    public String deleteOneUser(@PathVariable Long userId) {
-        userService.delete(userId);
-        return "redirect:/users";
-    }
+	@PostMapping("/users/{userId}/delete")
+	public String deleteOneUser(@PathVariable Long userId) {
+		userService.delete(userId);
+		return "redirect:/users";
+	}
 
 }
